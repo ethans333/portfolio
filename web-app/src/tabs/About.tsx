a14a32544c687cd3fa7b49e06fa482d9832f7a9c
estein <ethanmstn33@gmail.com>
2025-08-18 12:48:37 -0400

    Fixing typo

--- web-app/src/tabs/About.tsx
+++ web-app/src/tabs/About.tsx
@@ -6,24 +6,17 @@ export default function About() {
   return (
     <div className="py-2">
       <p>
-        Welcome to my personal portfolio! I'm Ethan Stein, a Software Engineer
-        and recent Computer Science graduate from the University of Central
-        Florida. Feel free to browser around and see some of my projects and
-        industry experience.
+        Welcome to my personal portfolio! I'm Ethan Stein, a Software Engineer and recent Computer Science graduate from
+        the University of Central Florida. Feel free to browse around and see some of my projects and industry
+        experience!
         <br />
         <br />
-        I'm{" "}
-        <p className="italic inline">
-          currently looking for Software Engineering roles
-        </p>
-        . If you have any questions or would like to connect, feel free to{" "}
-        <a href="mailto:ethanmstn33@gmail.com">reach out!</a>
+        I'm <p className="italic inline">currently looking for Software Engineering roles</p>. If you have any questions
+        or would like to connect, feel free to <a href="mailto:ethanmstn33@gmail.com">reach out!</a>
       </p>
       <div className="flex justify-center gap-6 mt-6">
         <a href="https://github.com/ethans333">Github</a>
-        <a href="https://www.linkedin.com/in/ethan-stein-231527256/">
-          LinkedIn
-        </a>
+        <a href="https://www.linkedin.com/in/ethan-stein-231527256/">LinkedIn</a>
         <a className="cursor-pointer" onClick={handleOpenPdf}>
           Resume
         </a>
