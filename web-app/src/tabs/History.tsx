9eec452b707bae99c12fb6c0fdc173994bc6b28a
estein <ethanmstn33@gmail.com>
2025-08-30 13:19:03 -0400

    Added enludio to work history

--- web-app/src/tabs/History.tsx
+++ web-app/src/tabs/History.tsx
@@ -1,14 +1,17 @@
+import { TextScramble } from "@/components/motion-primitives/hidden-word-text-scramble";
 import Badges from "@/components/ui/badges";
+import { Tilt } from "@/components/motion-primitives/tilt";
 
 export default function History() {
   return (
     <div className="flex flex-col items-center justify-center py-10">
+      <CurrentPosition companyName="Enludio" startDate="September 2025" />
+      <VDotLine />
       <WorkHistory
         companyName="MAK Technologies"
         description="Contributed to a government-contracted real-time simulation project focused on enhancing user interaction with dynamic digital environments. Utilized Python and RabbitMQ to implement communication features between systems. Worked within a four-person Agile team, using Git and Jira to manage development workflows."
         startDate="June 2024"
         endDate="June 2025"
-        link="https://mak.com"
         skills={[
           "Python",
           "Dart",
@@ -27,10 +30,19 @@ export default function History() {
         description="Designed and developed Celly Auto SMS, a full-stack platform built with AWS, Python, and React. Created a dynamic workflow builder UI for automated messaging and integrated Amazon Lex for AI-driven chat functionality. Managed the entire project lifecycle independently, from planning through deployment."
         startDate="February 2023"
         endDate="June 2024"
-        link="https://techthinktank.com/"
         skills={["Python", "React", "JavaScript", "Tailwind CSS", "TypeScript", "AWS"]}
       />
     </div>
+  );
+}
+
+function WorkCard({ children }: { children: React.ReactNode }) {
+  return (
+    <Tilt rotationFactor={8} isRevese>
+      <div className="w-[90vw] xl:w-[37vw] max-w-[600px] px-7 pt-8 pb-7 border border-zinc-950/10 bg-white dark:border-zinc-50/10 dark:bg-zinc-900 rounded-[12px]">
+        {children}
+      </div>
+    </Tilt>
   );
 }
 
@@ -45,16 +57,10 @@ function WorkHistory({
   description: string;
   startDate: string;
   endDate: string;
-  link: string;
   skills: string[];
 }) {
   return (
-    <div
-      style={{
-        borderRadius: "12px",
-      }}
-      className="w-[90vw] xl:w-[37vw] max-w-[600px] px-7 pt-8 pb-7 border border-zinc-950/10 bg-white dark:border-zinc-50/10 dark:bg-zinc-900"
-    >
+    <WorkCard>
       <div className="lg:flex justify-between items-center">
         <h2 className="mb-1">{companyName}</h2>
         <p className="opacity-50 mt-auto pb-1">
@@ -65,7 +71,35 @@ function WorkHistory({
       <div className="mt-2.5">
         <Badges badges={skills} />
       </div>
-    </div>
+    </WorkCard>
+  );
+}
+
+function CurrentPosition({ companyName, startDate }: { companyName: string; startDate: string }) {
+  return (
+    <WorkCard>
+      <div className="lg:flex justify-between items-center">
+        <h2 className="mb-1">{companyName}</h2>
+        <p className="opacity-50 mt-auto pb-1">{startDate}</p>
+      </div>
+      <p className="py-2">
+        <TextScramble className="font-mono text-sm line-clamp-3" duration={Infinity} characterSet=". ">
+          .Incididunt non officia cillum incididunt et amet eu nulla in duis deserunt labore. Amet nulla sit sit
+          consectetur dolore occaecat reprehenderit voluptate aliqua. Nostrud occaecat dolor sint Lorem id sint aliqua
+          ullamco aliquip laborum est laborum esse cupidatat. Id eiusmod tempor cillum non nostrud fugiat id duis. Sint
+          do in ut cillum labore ad magna. Aute pariatur anim duis exercitation ad pariatur cillum esse in labore.
+        </TextScramble>
+        <br />
+        <div className="flex space-x-10">
+          <TextScramble className="font-mono text-sm" duration={Infinity} characterSet=". ">
+            .xxxxxxxxxxxxxx
+          </TextScramble>
+          <TextScramble className="font-mono text-sm" duration={Infinity} characterSet=". ">
+            .ipsum
+          </TextScramble>
+        </div>
+      </p>
+    </WorkCard>
   );
 }
 
